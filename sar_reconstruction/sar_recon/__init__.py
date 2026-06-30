32783e79de5fb6aaf1459e1a3188bcafa0da640c
Lucas Ayub <120483658+lucas-ayub@users.noreply.github.com>
2026-06-30 15:57:02 +0200

    Add new plotting functions to __init__.py

--- sar_reconstruction/sar_recon/__init__.py
+++ sar_reconstruction/sar_recon/__init__.py
@@ -4,7 +4,7 @@ from .config import (
     SystemParams, Scene, ArrayGeometry, ExperimentConfig,
     integration_time, build_time_axis,
     prf_from_fixed, prf_from_dpca,
-    make_diff_config, make_dpca_config, CONFIG_FACTORIES,
+    make_diff_config, make_dpca_config, CONFIG_FACTORIES, SCENE_PRESETS,
 )
 from .geometry import PlatformTracks, build_platform_tracks
 from .signal_model import getRawData1D, generate_reference, generate_channels
@@ -12,15 +12,19 @@ from .reconstruction import (
     GetCoeffNu, GetInversionFilters, ReconstructSignalNumeri, reconstruct,
 )
 from .analysis import ReconResult, zoom1Dpeak, matched_filter, analyze
-from .plotting import plot_combined, plot_polyfit_diagnostic, plot_geometry_3d
+from .plotting import (
+    plot_combined, plot_polyfit_diagnostic, plot_geometry_3d,
+    plot_scene_points, plot_scene_points_3d, enable_latex_fonts,
+)
 
 __all__ = [
     "SystemParams", "Scene", "ArrayGeometry", "ExperimentConfig",
     "integration_time", "build_time_axis", "prf_from_fixed", "prf_from_dpca",
-    "make_diff_config", "make_dpca_config", "CONFIG_FACTORIES",
+    "make_diff_config", "make_dpca_config", "CONFIG_FACTORIES", "SCENE_PRESETS",
     "PlatformTracks", "build_platform_tracks",
     "getRawData1D", "generate_reference", "generate_channels",
     "GetCoeffNu", "GetInversionFilters", "ReconstructSignalNumeri", "reconstruct",
     "ReconResult", "zoom1Dpeak", "matched_filter", "analyze",
     "plot_combined", "plot_polyfit_diagnostic", "plot_geometry_3d",
+    "plot_scene_points", "plot_scene_points_3d", "enable_latex_fonts",
 ]
